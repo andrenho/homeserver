a9bcbeed054f65e1cab002ae8874aacbfd47f22f
André Wagner <andre.nho@gmail.com>
2022-01-05 06:41:58 -0300

    .

--- scripts/run_containers.sh
+++ scripts/run_containers.sh
@@ -4,6 +4,6 @@ set -ex  # stop at errors
 
 docker-compose build --no-cache
 
-cp service/docker-compose-app.service /etc/systemd/system/
+cp arch/docker-compose-app.service /etc/systemd/system/
 systemctl restart docker-compose-app
 systemctl daemon-reload
--- scripts/user_config.sh
+++ scripts/user_config.sh
@@ -6,3 +6,10 @@ set -ex  # stop at errors
 
 usermod -aG sudo andre
 usermod -aG docker andre
+
+# Copy configuration files to home
+cp -p arch/.vimrc /home/andre/
+cp -p arch/.bashrc /home/andre/
+cp -p arch/.tmux.conf /home/andre/
+cp -p arch/.dir_colors /home/andre/
+cp -p arch/.inputrc /home/andre/.inputrc
