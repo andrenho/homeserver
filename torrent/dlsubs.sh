2db5d045d1d927856475b3b693a4aed93075fcfc
André Wagner <andre.nho@gmail.com>
2022-01-06 13:00:26 -0300

    .

--- torrent/dlsubs.sh
+++ torrent/dlsubs.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-echo "### Download subtitles for $1" > /downloads/autorun.log
-for f in "$1"/*; do subdl --lang=eng --download=most-downloaded --existing=overwrite "$f"; done >> /downloads/autorun.log 2>&1
-for f in "$1"/*; do subdl --lang=pob --download=most-downloaded --existing=overwrite "$f"; done >> /downloads/autorun.log 2>&1
+echo >> /downloads/autorun.log
+echo "### Download subtitles for $1" >> /downloads/autorun.log
+echo >> /downloads/autorun.log
+subdl --lang=eng --download=most-downloaded --existing=overwrite "$1" >> /downloads/autorun.log 2>&1
+subdl --lang=pob --download=most-downloaded --existing=overwrite "$1" >> /downloads/autorun.log 2>&1
