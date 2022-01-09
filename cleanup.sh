82348bb7385c90d7fb7c4468dbd6d9e04fb9e945
André Wagner <andre.nho@gmail.com>
2022-01-09 16:48:58 -0300

    .

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,10 @@
 #/bin/sh
 
 sudo systemctl stop docker-compose-app
+docker-compose stop
 docker system prune --volumes -f
+docker network rm $(docker network ls | grep "bridge" | awk '/ / { print $1 }')
+docker rm $(docker ps -qa --no-trunc --filter "status=exited")
+rm -rf ~/.cache/*
 sudo pacman -Scc --noconfirm
+sudo journalctl --vacuum-size=20M
--- torrent/dlsubs.sh
+++ torrent/dlsubs.sh
@@ -4,4 +4,4 @@ echo >> /downloads/autorun.log
 echo "### Download subtitles for $1" >> /downloads/autorun.log
 echo >> /downloads/autorun.log
 subdl --lang=eng --download=most-downloaded --existing=overwrite "$1" >> /downloads/autorun.log 2>&1
-subdl --lang=pob --download=most-downloaded --existing=overwrite "$1" >> /downloads/autorun.log 2>&1
+#subdl --lang=pob --download=most-downloaded --existing=overwrite "$1" >> /downloads/autorun.log 2>&1
