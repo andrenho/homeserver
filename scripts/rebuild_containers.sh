56d949b91c8d188e6c6ef9f11dd9299d1478dbed
André Wagner <andre.nho@gmail.com>
2022-01-14 11:08:25 -0300

    .

--- scripts/rebuild_containers.sh
+++ scripts/rebuild_containers.sh
@@ -3,6 +3,7 @@
 docker-compose stop
 docker-compose rm -f
 docker-compose up --build --force-recreate --no-deps -d pihole
+docker-compose build --no-cache dev
 docker-compose pull
 docker-compose build --no-cache
 docker-compose up --build --force-recreate --no-deps -d
