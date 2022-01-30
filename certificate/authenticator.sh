a558e1ac1ba456977abf7da63e8d34b0aa12b4d3
André Wagner <andre.nho@gmail.com>
2022-01-30 07:28:08 -0300

    .

--- certificate/authenticator.sh
+++ certificate/authenticator.sh
@@ -14,6 +14,11 @@ cat <<EOF  > createrec.json
 }
 EOF
 
+echo "Finding hosted zone..."
+DOMAIN=$(expr match "$CERTBOT_DOMAIN" '.*\.\(.*\..*\)')
+HOSTED_ZONE=$(aws route53 list-hosted-zones-by-name | jq --arg name "$DOMAIN." -r '.HostedZones | .[] | select(.Name=="\($name)") | .Id')
+echo "Found hosted zone $HOSTED_ZONE."
+
 echo "Creating authorization record '_acme-challenge.$CERTBOT_DOMAIN' with value '$CERTBOT_VALIDATION'..."
 ID=$(aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE --change-batch file://createrec.json | jq -r ".ChangeInfo.Id")
 
