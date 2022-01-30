0162318c26bf4961327a9820aa12508bdf1c3a53
André Wagner <andre.nho@gmail.com>
2022-01-30 07:15:37 -0300

    .

--- certificate/authenticator.sh
+++ certificate/authenticator.sh
@@ -15,9 +15,9 @@ cat <<EOF  > createrec.json
 EOF
 
 echo "Creating authorization record '_acme-challenge.$CERTBOT_DOMAIN' with value '$CERTBOT_VALIDATION'..."
-ID=$(aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE --change-batch file://createrec.json | jq ".ChangeInfo.Id")
+ID=$(aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE --change-batch file://createrec.json | jq -r ".ChangeInfo.Id")
 
-echo "Record '_acme-challenge.$CERTBOT_DOMAIN' with value '$CERTBOT_VALIDATION'... created with request id $ID. Now waiting for change..."
+echo "Record '_acme-challenge.$CERTBOT_DOMAIN' with value '$CERTBOT_VALIDATION'... created with request id '$ID'. Now waiting for change..."
 
 aws route53 wait resource-record-sets-changed --id "$ID"
-echo "Change $ID completed."
+echo "Change '$ID' completed."
