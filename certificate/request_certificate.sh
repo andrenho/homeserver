5e8d513ce198113dc682c2d45ec379d8b94cc901
André Wagner <andre.nho@gmail.com>
2022-02-01 16:47:30 -0300

    .

--- certificate/request_certificate.sh
+++ certificate/request_certificate.sh
@@ -4,7 +4,8 @@ sudo mkdir /root/.aws
 sudo cp /home/andre/.aws/* /root/.aws
 sudo certbot certonly --agree-tos --email andre.nho@gmail.com \
   --preferred-challenges=dns \
-  --manual --manual-auth-hook /home/andre/homeserver/certificate/authenticator.sh \
-  --manual-cleanup-hook /home/andre/homeserver/certificate/cleanup.sh \
+  --manual --manual-auth-hook $(pwd)/authenticator.sh \
+  --manual-cleanup-hook $(pwd)/cleanup.sh \
   -d homeserver.gamesmith.uk -d '*.homeserver.gamesmith.uk' -v \
+  --staging \
   $@
