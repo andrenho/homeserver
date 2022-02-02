56ab7d32e45fb4eba1ce44295a946417afa70106
André Wagner <andre.nho@gmail.com>
2022-02-02 17:25:39 -0300

    .

--- certificate/request_certificate.sh
+++ certificate/request_certificate.sh
@@ -2,25 +2,16 @@
 
 set -x
 
-mkdir -p /ssl
+certbot certonly -n --agree-tos --email ${EMAIL} --manual-public-ip-logging-ok \
+  --preferred-challenges=dns \
+  --manual --manual-auth-hook $(pwd)/authenticator.sh \
+  --manual-cleanup-hook $(pwd)/cleanup.sh \
+  -d ${DOMAIN} \
+  ${EXTRA_PARAMS}
 
-if [ ! -f /ssl/fullchain.pem ]; then
-
-  certbot certonly -n --agree-tos --email ${EMAIL} --manual-public-ip-logging-ok \
-    --preferred-challenges=dns \
-    --manual --manual-auth-hook $(pwd)/authenticator.sh \
-    --manual-cleanup-hook $(pwd)/cleanup.sh \
-    --fullchain-path /ssl/standalone.pub.key \
-    --key-path /ssl/standalone.priv.key \
-    -d ${DOMAIN} \
-    ${EXTRA_PARAMS}
-
-  certbot certonly -n --agree-tos --email ${EMAIL} --manual-public-ip-logging-ok \
-    --preferred-challenges=dns \
-    --manual --manual-auth-hook $(pwd)/authenticator.sh \
-    --manual-cleanup-hook $(pwd)/cleanup.sh \
-    --fullchain-path /ssl/wildcard.pub.key \
-    --key-path /ssl/wildcard.priv.key \
-    -d '*.'"${DOMAIN}"'' \
-    ${EXTRA_PARAMS}
-fi
+certbot certonly -n --agree-tos --email ${EMAIL} --manual-public-ip-logging-ok \
+  --preferred-challenges=dns \
+  --manual --manual-auth-hook $(pwd)/authenticator.sh \
+  --manual-cleanup-hook $(pwd)/cleanup.sh \
+  -d '*.'"${DOMAIN}"'' \
+  ${EXTRA_PARAMS}
